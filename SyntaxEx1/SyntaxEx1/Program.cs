6e621b8f89c6cdfd7c225c74071f1567e54a3bbb
DJ Bates <blooviper@gmail.com>
2022-08-09 10:58:59 -0600

    initial commit

---
+++ SyntaxEx1/SyntaxEx1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SyntaxEx1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // inferred typing
+            var answer = 4;
+            // string interpolation and ternary operator
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine";
+            Console.WriteLine(response);
+
+
+            // int answer = 4;
+            //string response;
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + " is greater than or equal to nine";
+            //}
+        }
+    }
+}
